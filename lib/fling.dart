bb72c1caac4ad03695e1728d8f67dee0dc39da72
Garrit Franke <garrit@slashdev.space>
2021-05-16 13:38:23 +0200

    chore(fling): flatten view hirarchy

--- lib/fling.dart
+++ lib/fling.dart
@@ -50,74 +50,81 @@ class _TodoPageState extends State<TodoPage> {
   Widget build(BuildContext context) {
     TodoModel model = context.watch<TodoModel>();
 
+    Widget _buildDeleteButton() {
+      return IconButton(
+          icon: Icon(Icons.delete),
+          color: Colors.red,
+          onPressed: () => model.deleteChecked());
+    }
+
+    Widget _buildAppBar() {
+     return AppBar(
+          title: Text(widget.title),
+          actions: [_buildDeleteButton()],
+        ) 
+    }
+
+    Widget _buildItemTextField() {
+      return Container(
+        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+        // TODO: use subscribed model in state
+        child: Consumer<TodoModel>(
+          builder: (context, model, child) {
+            return TextField(
+              controller: newTodoController,
+              onSubmitted: (value) {
+                model.addItem(value);
+                newTodoController.clear();
+              },
+              decoration: InputDecoration(
+                  hintText: "Nudeln",
+                  border: OutlineInputBorder(),
+                  labelText: "Item hinzufügen"),
+            );
+          },
+        ),
+      );
+    }
+
+    Widget _buildItemList() {
+      return Expanded(
+          child: StreamBuilder(
+              stream: model.items,
+              builder: (context, snapshot) {
+                if (snapshot.hasError || !snapshot.hasData) {
+                  return Text("Etwas ist Schiefgegangen: ${snapshot.error}");
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+
+                List<Item> items = snapshot.data.docs
+                    .map<Item>((QueryDocumentSnapshot doc) =>
+                        Item.fromMap({"id": doc.id, ...doc.data()}))
+                    .toList();
+
+                return ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      Item item = items.elementAt(index);
+                      return Dismissible(
+                          onDismissed: (direction) => model.deleteItem(item),
+                          key: Key(item.id),
+                          child: ItemView(item));
+                    });
+              }));
+    }
+
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.delete),
-                color: Colors.red,
-                onPressed: () => model.deleteChecked())
-          ],
-        ),
+        appBar: _buildAppBar(),
         body: Center(
           child: SizedBox(
             width: 600.0,
             child: Column(
-              children: [
-                Container(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-                  // TODO: use subscribed model in state
-                  child: Consumer<TodoModel>(
-                    builder: (context, model, child) {
-                      return TextField(
-                        controller: newTodoController,
-                        onSubmitted: (value) {
-                          model.addItem(value);
-                          newTodoController.clear();
-                        },
-                        decoration: InputDecoration(
-                            hintText: "Nudeln",
-                            border: OutlineInputBorder(),
-                            labelText: "Item hinzufügen"),
-                      );
-                    },
-                  ),
-                ),
-                Expanded(
-                    child: StreamBuilder(
-                        stream: model.items,
-                        builder: (context, snapshot) {
-                          if (snapshot.hasError || !snapshot.hasData) {
-                            return Text(
-                                "Etwas ist Schiefgegangen: ${snapshot.error}");
-                          }
-
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return CircularProgressIndicator();
-                          }
-
-                          List<Item> items = snapshot.data.docs
-                              .map<Item>((QueryDocumentSnapshot doc) =>
-                                  Item.fromMap({"id": doc.id, ...doc.data()}))
-                              .toList();
-
-                          return ListView.builder(
-                              itemCount: items.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                Item item = items.elementAt(index);
-                                return Dismissible(
-                                    onDismissed: (direction) =>
-                                        model.deleteItem(item),
-                                    key: Key(item.id),
-                                    child: ItemView(item));
-                              });
-                        })),
-              ],
+              children: [_buildItemTextField(), _buildItemList()],
             ),
           ),
         ),
