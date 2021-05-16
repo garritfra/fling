6545b7f9cfc25fee480b19d8110fdbb888c73d6e
Garrit Franke <garrit@slashdev.space>
2021-05-16 14:34:35 +0200

    chore: extract shopping list component

--- lib/fling.dart
+++ lib/fling.dart
@@ -1,8 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'TodoModel.dart';
-import 'item.dart';
+import 'shopping_list.dart';
 
 class FlingApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -14,121 +11,7 @@ class FlingApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: TodoPage(title: 'Fling'),
-    );
-  }
-}
-
-class TodoPage extends StatefulWidget {
-  TodoPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _TodoPageState createState() => _TodoPageState();
-}
-
-class _TodoPageState extends State<TodoPage> {
-  final newTodoController = TextEditingController();
-  ScrollController _scrollController;
-
-  @override
-  void initState() {
-    _scrollController = ScrollController(keepScrollOffset: true);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is removed from the
-    // widget tree.
-    newTodoController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    TodoModel model = context.watch<TodoModel>();
-
-    Widget _buildDeleteButton() {
-      return IconButton(
-          icon: Icon(Icons.delete),
-          color: Colors.red,
-          onPressed: () => model.deleteChecked());
-    }
-
-    Widget _buildAppBar() {
-      return AppBar(
-        title: Text(widget.title),
-        actions: [_buildDeleteButton()],
-      );
-    }
-
-    Widget _buildItemTextField() {
-      return Container(
-        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-        // TODO: use subscribed model in state
-        child: Consumer<TodoModel>(
-          builder: (context, model, child) {
-            return TextField(
-              controller: newTodoController,
-              onSubmitted: (value) {
-                model.addItem(value);
-                newTodoController.clear();
-              },
-              decoration: InputDecoration(
-                  hintText: "Nudeln",
-                  border: OutlineInputBorder(),
-                  labelText: "Item hinzufügen"),
-            );
-          },
-        ),
-      );
-    }
-
-    Widget _buildItemList() {
-      return Expanded(
-          child: StreamBuilder(
-              stream: model.items,
-              builder: (context, snapshot) {
-                if (snapshot.hasError || !snapshot.hasData) {
-                  return Text("Etwas ist Schiefgegangen: ${snapshot.error}");
-                }
-
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                }
-
-                List<Item> items = snapshot.data.docs
-                    .map<Item>((QueryDocumentSnapshot doc) =>
-                        Item.fromMap({"id": doc.id, ...doc.data()}))
-                    .toList();
-
-                return ListView.builder(
-                    itemCount: items.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      Item item = items.elementAt(index);
-                      return Dismissible(
-                          onDismissed: (direction) => model.deleteItem(item),
-                          key: Key(item.id),
-                          child: ItemView(item));
-                    });
-              }));
-    }
-
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: _buildAppBar(),
-        body: Center(
-          child: SizedBox(
-            width: 600.0,
-            child: Column(
-              children: [_buildItemTextField(), _buildItemList()],
-            ),
-          ),
-        ),
-      ),
+      home: ShoppingList(title: 'Fling'),
     );
   }
 }
