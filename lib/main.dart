5af737b0f26527d5aaa86feddd31c5f5af96fbf6
Garrit Franke <garrit@slashdev.space>
2022-12-11 13:02:13 +0100

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'pages/login.dart';
 import 'pages/home.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const FlingApp());
 }
 
