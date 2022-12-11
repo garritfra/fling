c580dfd2d956260751227de55faac6327d62df15
Garrit Franke <garrit@slashdev.space>
2022-12-11 12:22:54 +0100

    Add home and login routes

---
+++ lib/pages/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('This is the login route'),
+          ],
+        ),
+      ),
+    );
+  }
+}
